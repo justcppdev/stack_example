660831f621272c561352a22c2439310a0977655f
Elina Gabitova <elinagabitovaa@gmail.com>
2018-02-11 00:49:48 +0500

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -3,20 +3,3 @@
 
 #include "stack.hpp"
 
-int main()
-{
-    stack<int> s;
-    for( std::string line; std::getline( std::cin, line ); ) {
-        std::istringstream stream{ line };
-        if( char op; stream >> op ) {
-            if( op == 'e' ) {
-                std::cout << std::boolalpha << s.empty();
-            } 
-            else if( op == 'q' ) {
-                break;
-            }
-        }
-    }
-    
-    return 0;
-}
